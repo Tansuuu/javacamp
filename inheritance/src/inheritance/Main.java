a96aa4dedf790571112dfa99001ad8d6d1512e85
Tansu Çağla Tekin <tansu_2001@hotmail.com>
2021-05-06 19:01:33 +0300

    06.05.21

---
+++ inheritance/src/inheritance/Main.java
@@ -0,0 +1,28 @@
+package inheritance;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		IndividualCustomer engin = new IndividualCustomer();
+		engin.customerNumber = "12345";
+		
+		
+		CorporateCustomer hepsiBurada = new CorporateCustomer();
+		hepsiBurada.customerNumber = "678910";
+		
+		SendikaCustomer abc = new SendikaCustomer();
+		abc.customerNumber = "99999";
+		
+		CustomerManager customerManager = new CustomerManager();
+		
+//		customerManager.add(hepsiBurada);
+//		customerManager.add(engin);
+//		customerManager.add(sendikaCustomer);
+		
+		Customer[] customers = {engin, abc, hepsiBurada};
+		
+		customerManager.addMultiple(customers);
+	}
+
+}
---
+++ oopIntro/src/oopIntro/Main.java
@@ -0,0 +1,53 @@
+package oopIntro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Product product1 = new Product(1, "Lenovo V14", 15000, "16 GB Ram", 10);  //referans oluþturma, intance
+		
+		Product product2 = new Product();
+		product2.setId(2);
+		product2.setName("Lenovo V15");
+		product2.setDetail("16 GB Ram");
+		product2.setDiscount(10);
+		product2.setUnitPrice(1600);
+		
+		System.out.println(product2.getUnitPriceAfterDiscount());
+		
+		
+		//Product product3 = new Product();
+		//
+		//
+		//Product[] products = {product1, product2, product3};
+		//
+		//for (Product product : products) {
+		//	System.out.println(product.name);
+		//}
+		//
+		//System.out.println(products.length);
+		//
+		//
+		Category category1 = new Category();
+		category1.setId (1);
+		category1.setName ("Bilgisayar");
+		
+		Category category2 = new Category();
+		category2.setId (2);
+		category2.setName ("Ev/Bahçe");
+		
+		System.out.println(category1.getName());
+		System.out.println(category2.getName());
+		
+		
+		//ProductManager productManager = new ProductManager();
+		//
+		//productManager.addToCart(product1);
+		//
+		//productManager.addToCart(product2);
+		//
+		//productManager.addToCart(product3);
+		
+	}
+
+}
